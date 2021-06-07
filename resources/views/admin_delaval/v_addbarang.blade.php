9ef278676b3dca5fc0ca0cb2d502cb7172c6b185
alfin2312 <alfin.mulya1999@gmail.com>
2021-06-07 21:19:56 +0700

    mengembalikan codingan abdelas

--- resources/views/admin_delaval/v_addbarang.blade.php
+++ resources/views/admin_delaval/v_addbarang.blade.php
@@ -68,7 +68,7 @@
                     <div class="input-group-prepend">
                         <span class="input-group-text">@</span>
                     </div>
-                    <input type="text" id="rupiah" name="harga" placeholder="Harga Barang" class="form-control" value="{{ old('harga') }}">
+                    <input type="text" name="harga" placeholder="Harga Barang" class="form-control" value="{{ old('harga') }}">
                     <div class="text-danger">
                         @error('harga')
                         {{ $message }}
@@ -111,30 +111,4 @@
     </div><!-- /.container-fluid -->
 </section>
 
-<script type="text/javascript">
-    var rupiah = document.getElementById('rupiah');
-    rupiah.addEventListener('keyup', function(e) {
-        // tambahkan 'Rp.' pada saat form di ketik
-        // gunakan fungsi formatRupiah() untuk mengubah angka yang di ketik menjadi format angka
-        rupiah.value = formatRupiah(this.value, 'Rp. ');
-    });
-
-    /* Fungsi formatRupiah */
-    function formatRupiah(angka, prefix) {
-        var number_string = angka.replace(/[^,\d]/g, '').toString(),
-            split = number_string.split(','),
-            sisa = split[0].length % 3,
-            rupiah = split[0].substr(0, sisa),
-            ribuan = split[0].substr(sisa).match(/\d{3}/gi);
-
-        // tambahkan titik jika yang di input sudah menjadi angka ribuan
-        if (ribuan) {
-            separator = sisa ? '.' : '';
-            rupiah += separator + ribuan.join('.');
-        }
-
-        rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
-        return prefix == undefined ? rupiah : (rupiah ? 'Rp. ' + rupiah : '');
-    }
-</script>
 @endsection
--- resources/views/admin_delaval/v_dashboard.blade.php
+++ resources/views/admin_delaval/v_dashboard.blade.php
@@ -3,28 +3,11 @@
 
 @section('content')
 
-<style>
-  #watch {
-    color: black;
-    position: relative;
-    height: 2em;
-    width: 3.8em;
-    margin: auto;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    right: 0;
-    font-size: 30px;
-    float: left;
-  }
-</style>
-
 <div class="content-header">
   <div class="container-fluid">
     <div class="row mb-2">
       <div class="col-sm-6">
         <h1 class="m-0">Dashboard</h1><br>
-        <div id="watch"></div>
       </div><!-- /.col -->
       <div class="col-sm-6">
         <ol class="breadcrumb float-sm-right">
@@ -84,22 +67,5 @@
     </div><!-- /.container-fluid -->
 
 </section>
-
-<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-<script type="text/javascript">
-  $(document).ready(function() {
-    function clock() {
-      var now = new Date();
-      var secs = ('0' + now.getSeconds()).slice(-2);
-      var mins = ('0' + now.getMinutes()).slice(-2);
-      var hr = now.getHours();
-      var Time = hr + ":" + mins + ":" + secs;
-      document.getElementById("watch").innerHTML = Time;
-      requestAnimationFrame(clock);
-    }
-
-    requestAnimationFrame(clock);
-  });
-</script>
 
 @endsection
